4c687f07cd8351afe1e348219b20f48149607441
mluan23 <luanmrun20@gmail.com>
2024-11-22 17:35:50 -0500

    Can scrape first page of college names

--- scraper.py
+++ scraper.py
@@ -1,15 +1,28 @@
 from bs4 import BeautifulSoup
 import requests
 
+def scrape_page(url):
+    # need to do this or get 403
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; DEVICE INFO) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Mobile Safari/537.36 Edge/12.<OS Build #>'}
+    response = requests.get(url, headers=headers) # get the url
+    # if(response != 200): # 200 is success
+    #     return None
+    bs = BeautifulSoup(response.content, 'html.parser') # parse the HTML of the response
+    colleges = []
+    #print(bs.prettify())
+    college_elements = bs.findAll('div', class_='card search-result') # get each college
+    for college in college_elements:
+        name = college.find('h2').text # get the college name
+        colleges.append(name)
+    return colleges # return colleges, and bs if want to reuse it
 
-base_url = "https://www.niche.com/colleges/search/best-colleges/"
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    base_url = "https://www.niche.com/colleges/search/best-colleges/"
+    c = scrape_page(base_url)
+    for i in c:
+       print(i)
 
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
