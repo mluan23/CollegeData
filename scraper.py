a9cf8f83f79af6cadf39b49f26d37a205c9cbd07
mluan23 <luanmrun20@gmail.com>
2025-01-05 22:28:00 -0500

    Added lots more fields to read, included error handling; still have bug with writing to file, after parsing pages that are not cached

--- scraper.py
+++ scraper.py
@@ -22,10 +22,11 @@ u_bound = 12
 requests_cache.install_cache('niche_data_colleges', expire_after=3600) # caching responses so less requests to server
 ua = UserAgent()
 proxies = []
-cache_time = 10800 # Cache duration in seconds (3 hours)
-lock = threading.Lock()
+cache_time = 10800 * 8 * 7 # Cache duration in seconds (1 week)
+#lock = threading.Lock()
 #colleges = []
 filename = "colleges.csv"
+start_time = time.time()
 #count = 1 # count the num colleges
 # options = Options()
 # options.headless = True # Run in headless mode
@@ -65,21 +66,14 @@ def scrape_page(url):
     colleges = []
 
     colleges_written, initial_count = get_current_written(filename)
-    headers, proxy = get_headers_and_proxy()
     initial_count += 1
-    # proxy = random.choice(proxies)
-    response = requests.get(url, headers=headers, proxies=proxy, timeout=10) # get the url
+    response = make_request(url)
     if response.status_code != 200:  # 403 is forbidden, try a new proxy / header
-        print(str(response.status_code) + " error for original link, agent:" + headers["User-Agent"])
-        user_agents.remove(headers["User-Agent"])
-        if response.status_code == 404 or len(headers) == 0:
+        if response.status_code == 404 or len(user_agents) == 0:
             print("Retry again later")
             return None
-
-        time.sleep(random.uniform(l_bound, u_bound))
-        return scrape_page(url)
-    if response.from_cache:
-        print("ok")
+        scrape_page(url)
+        return None
     soup = BeautifulSoup(response.content, 'html.parser')  # parse the HTML of the response
     college_elements = soup.findAll('div', class_='card search-result') # get each college
     for college in college_elements:
@@ -114,28 +108,40 @@ def scrape_all_pages(url):
         else:
             url = None
     return all_colleges
-def get_college_info(url, college, count):  # get info about 1 college
-    college_info = {'name': college.find('h2').text, 'url': url}
+
+def make_request(url):
     headers, proxy = get_headers_and_proxy()
     response = requests.get(url, headers=headers, proxies=proxy)
-    print("getting info for " + url + " , agent:" + headers["User-Agent"])
-    if response.status_code != 200:  # 403 is forbidden, try a new proxy / header
+    if response.status_code != 200:
         print(str(response.status_code) + " error for " + url + " , agent:" + headers["User-Agent"])
         user_agents.remove(headers["User-Agent"])
         time.sleep(random.uniform(l_bound, u_bound))
+    end = time.time()
+    elapsed = end-start_time
+    print(elapsed)
+    return response
+def get_college_info(url, college, count):  # get info about 1 college
+    college_info = {'name': college.find('h2').text, 'url': url}
+    response = make_request(url)
+    if response.status_code != 200:
         get_college_info(url, college, count)
         return None
+    print("getting info for " + college.find('h2').text)
     college_info['rank'] = count
     soup = BeautifulSoup(response.content, 'html.parser')
-    #cost = re.sub(r'[^\d]', '', cost)  # regex to make just the num
     college_info['net cost'] = find_net_cost(soup)  # add to the info
     college_info["median earnings"] = find_earnings(soup)
     college_info['location'] = find_location(soup)
+    college_info['student-faculty ratio'] = find_student_faculty_ratio(soup)
+    college_info['majors'] = find_majors(soup)
+    college_info['acceptance rate'] = find_acceptance_rate(soup)
+    college_info['application deadline'] = find_application_deadline(soup)
+    college_info['graduation rate'], college_info['employed 2 years post graduation'] \
+        = find_graduation_rate_and_employment(soup)
+    #college_info['employed 2 years post graduation'] = find_employment_rate_post_two_years(soup)
+    college_info['full-time undergraduates'] = find_enrolled(soup)
     #add_to_colleges(college_info)
-    if response.from_cache:
-        print("from cache")
-        pass
-    else:
+    if not response.from_cache:
         time.sleep(random.uniform(l_bound, u_bound))
     return college_info
 def find_net_cost(soup):
@@ -144,19 +150,6 @@ def find_net_cost(soup):
     return convert_to_num(cost)
 def find_location(soup):
     return soup.findAll('li', class_='postcard__attr postcard-fact')[1].text
-def convert_to_num(original):
-    num = re.sub(r'[^\d]', '', original)
-    return int(num)
-
-def get_current_written(filename):
-    try:
-        with open(filename, 'r', newline='') as file:
-            reader = csv.DictReader(file)
-            names = [row['name'] for row in reader]
-            return names[0:], len(names)
-    except FileNotFoundError:
-        return [], 0
-
 
 # used to just be 10th profile bucket on all, not sure why not now
 def find_earnings(soup):
@@ -170,19 +163,146 @@ def find_earnings(soup):
         except AttributeError:
             pass
     return convert_to_num(earnings)
+def find_student_faculty_ratio(soup):
+    buckets = soup.find_all('div', class_='profile__bucket--1')
+    ratio = 'unlisted'
+    for bucket in buckets:
+        try:
+            value_label = bucket.find('div', class_='scalar__label').find('span').text
+            if "Student" in value_label:
+                ratio = bucket.find('div', class_='scalar__value').find('span').text
+        except AttributeError:
+            pass
+    return ratio
+def find_majors(soup):
+    expansions = soup.find_all('div', class_='expansion-link')
+    majors_url = ''
+    for e in expansions:
+        try:
+            link_text = e.find('a', class_='expansion-link__text').find('span').text
+            if "See All Majors" in link_text:
+                majors_url = e.find('a', class_='expansion-link__text')['href']
+                break
+        except AttributeError:
+            pass
+    if majors_url == '':
+        return []
+    response = make_request(majors_url)
+    if response.status_code != 200:
+        find_majors(soup)
+        return None
+    if not response.from_cache:
+        time.sleep(random.uniform(l_bound, u_bound))
+    soup = BeautifulSoup(response.content, 'html.parser')
+    majors = soup.find_all('li', class_='majors-list-item')
+    list_majors = []
+    for major in majors:
+        list_majors.append(major.find('div', class_='majors-list-item-major').text)
+    return list_majors
+
+def find_acceptance_rate(soup):
+    labels = soup.find_all('div', class_='MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-sm-6 nss-1x0x05w')
+    for label in labels:
+        try:
+            if 'Acceptance Rate' in label.find('div', class_='scalar__label').find('span').text:
+                return int(convert_to_num(label.find('div', class_='scalar__value').text))
+        except AttributeError:
+            pass
+    return 0
+
+def find_application_deadline(soup):
+    d = soup.find('div', class_='MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-sm-6 block--admissions__application-deadline nss-1x0x05w')
+    return d.find('div', class_='scalar__value').find('span').text
+def find_enrolled(soup):
+    buckets = soup.find_all('div', class_='profile__bucket--1')
+    for bucket in buckets:
+        try:
+            label = bucket.find('div', class_='scalar__label').find('span').text
+            if 'Full-Time Enrollment' in label:
+                return convert_to_num(bucket.find('div', class_='scalar__value').find('span').text)
+        except AttributeError:
+            pass
+    return 0
+def find_graduation_rate_and_employment(soup):
+   # buckets = soup.find_all('div', class_='profile__bucket--2')
+    three = soup.find_all('div', class_='scalar--three')
+    g_rate = 0
+    e_rate = 0
+    for t in three:
+        try:
+            labels = t.find_all('div', class_='scalar__label')
+            for label in labels:
+                if 'Graduation Rate' in label.find('span').text:
+                    g_rate = convert_to_num(t.find('div', class_='scalar__value').find('span').text)
+                elif 'Employed 2 Years After Graduation' in label.find('span').text:
+                    e_rate = convert_to_num(t.find('div', class_='scalar__value').find('span').text)
+        except AttributeError:
+            pass
+    return g_rate, e_rate
+def find_employment_rate_post_two_years(soup):
+    buckets = soup.find_all('div', class_='profile__bucket--2')
+    for bucket in buckets:
+        try:
+            label = bucket.find_all('div', class_='scalar__label')[2].find('span').text
+            if 'Employed 2 Years After Graduation' in label:
+                return convert_to_num(bucket.find('div', class_='scalar__value').find('span').text)
+        except AttributeError:
+            pass
+        except IndexError:
+            pass
+    return 0
+def convert_to_num(original):
+    num = re.sub(r'[^\d]', '', original)
+    return int(num)
+
 # def add_to_colleges(college_info):
 #     with lock:
 #         colleges.append(college_info)
+
+
+def get_current_written(filename):
+    try:
+        with open(filename, 'r', newline='') as file:
+            reader = csv.DictReader(file)
+            names = [row['name'] for row in reader]
+            return names[0:], len(names)
+    except FileNotFoundError:
+        return [], 0
 def write_to_file(some_colleges, namefile):
     with open(namefile, 'a', newline='') as file:
-        fieldnames = ['rank', 'name', 'url', 'net cost', 'median earnings', 'location']
+        fieldnames = some_colleges[0].keys()
+        # fieldnames = ['rank', 'name', 'url', 'net cost', 'median earnings', 'location', 'student-faculty ratio',
+        #               'majors', 'acceptance rate', 'application deadline']
         writer = csv.DictWriter(file, fieldnames=fieldnames)
         #writer = csv.writer(file)
         a, b = get_current_written(namefile)
         if b == 0:
             writer.writeheader()
-        for college in some_colleges:
-            writer.writerow(college)
+        for college in some_colleges: # we need to write each dict separately
+            try:
+                writer.writerow(college)
+            except AttributeError:
+                print(college)
+                pass
+
+
+def delete_row_by_number(name, row_number):
+    with open(name, mode='r') as file:
+        # Read the data into a list
+        reader = csv.reader(file)
+        rows = list(reader)
+
+    # Check if the row_number is valid
+    if row_number < 0 or row_number >= len(rows):
+        raise ValueError("Row number out of range")
+
+    # Delete the row by index
+    del rows[row_number]
+
+    with open(name, mode='w', newline='') as file:
+        # Write the updated data back to the CSV file
+        writer = csv.writer(file)
+        writer.writerows(rows)
 def get_headers_and_proxy():
     agent = random.choice(user_agents)
     proxy = get_rand_proxy()
@@ -233,39 +353,21 @@ def get_rand_proxy():
     proxies.extend(new_proxies)
     return random.choice(new_proxies)
 
-def delete_row_by_number(name, row_number):
-    with open(name, mode='r') as file:
-        # Read the data into a list
-        reader = csv.reader(file)
-        rows = list(reader)
-
-    # Check if the row_number is valid
-    if row_number < 0 or row_number >= len(rows):
-        raise ValueError("Row number out of range")
-
-    # Delete the row by index
-    del rows[row_number]
-
-    with open(name, mode='w', newline='') as file:
-        # Write the updated data back to the CSV file
-        writer = csv.writer(file)
-        writer.writerows(rows)
 
 
 # Retrieve a random index proxy (we need the index to delete it if not working)
 #def random_proxy():
  # return random.randint(0, len(proxies) - 1)
 def main():
+
     base_url = "https://www.niche.com/colleges/search/best-colleges/"
     #base_url = "https://www.niche.com/colleges/search/best-colleges/?page=5"
-    #get_current_written('colleges.csv')
-    # get_rand_proxy()
+
     #delete_row_by_number(filename, 2)
     c = scrape_some_pages(base_url, 1)
+    #c = scrape_all_pages(base_url)
     for i in c:
         print(i)
-    #write_to_file(c)
-    #print(UserAgent().random)
 
 
 
